4a6c579aa7f6f4781c066142a52edf209598bc0d
Olivera Leonardo <olezdev@gmail.com>
2025-10-16 15:58:17 -0300

    feat: update dependencies and configure TypeScript paths
    - Added @astrojs/check dependency to package.json
    - Added typescript dependency to package.json
    - Updated tsconfig.json to include baseUrl and paths for module resolution

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import path from 'node:path';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://olezdev.github.io',
   base: '/porfolio',
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src')
+      }
+    }
   }
 });
